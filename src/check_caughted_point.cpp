ed79a86a6d624e111aa66e71b215597a802071f6
Satoshi Otsubo <otsubo@jsk.imi.i.u-tokyo.ac.jp>
2017-03-23 18:00:30 +0900

    add test node

---
+++ src/check_caughted_point.cpp
@@ -0,0 +1,103 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <jsk_recognition_msgs/BoundingBox.h>
+#include <jsk_recognition_msgs/BoundingBoxArray.h>
+#include <geometry_msgs/PointStamped.h>
+#include <time.h>
+#include <vector>
+#include <boost/bind.hpp>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+
+ros::Publisher dist_pub;
+ros::Publisher nearest_marker_pub;
+//boost::mutex mutex_;
+
+
+
+void calculate(
+               const jsk_recognition_msgs::BoundingBox::ConstPtr& box,
+               const visualization_msgs::MarkerArray::ConstPtr& marker_array)
+{
+  //std::count<<"calculate"<<std::endl;
+  jsk_recognition_msgs::BoundingBox new_bbox_msg(*box);
+  //visualization_msgs::MarkerArray ma;
+  visualization_msgs::MarkerArray new_marker_msg(*marker_array);
+  visualization_msgs::Marker nearest_marker;
+  double dist;
+  std::vector<double> dist_vec;
+  double min_dist;
+  // ba.header = box->header;
+  // ba.pose = box->pose;
+  // ba.dimensions = box->dimensions;
+  //ma.markers.header = marker_array->header;
+  //ma.markers = marker_array->markers;
+
+  double b_x = new_bbox_msg.pose.position.x;
+  double b_y = new_bbox_msg.pose.position.y;
+  double b_z = new_bbox_msg.pose.position.z;
+  double m_x;
+  double m_y;
+  double m_z;
+  //ba.header = box->header;
+  //ba.box = box->box;
+  //ma.header = marker_array->header;
+  //ma.markers = marker_array->markers;
+
+  for(size_t i = 0; i < new_marker_msg.markers.size() ; i++) {
+    min_dist = -100;
+    m_x = new_marker_msg.markers[i].pose.position.x;
+    m_y = new_marker_msg.markers[i].pose.position.y;
+    m_z = new_marker_msg.markers[i].pose.position.z;
+    dist = sqrt(pow(m_x - b_x, 2.0) + pow(m_y - b_y, 2.0) + pow(m_z - b_z, 2.0));
+    if (dist < min_dist)
+      min_dist = dist;
+    nearest_marker = new_marker_msg.markers[i];
+  }
+  //Publish the data
+  dist_pub.publish (min_dist);
+  nearest_marker_pub.publish (nearest_marker);
+}
+
+
+int
+main(int argc, char** argv)
+{
+
+  //boost::mutex::scope_lock lock(mutex_);
+  //Initialize ROS
+  ros::init (argc, argv, "check_node");
+  ros::NodeHandle nh;
+  typedef message_filters::sync_policies::ApproximateTime<jsk_recognition_msgs::BoundingBox, visualization_msgs::MarkerArray> MySyncPolicy;
+  boost::shared_ptr<message_filters::Synchronizer<MySyncPolicy> > sync_;
+  //message filter
+  message_filters::Subscriber<jsk_recognition_msgs::BoundingBox> box_sub(nh, "input_box",1);
+  message_filters::Subscriber<visualization_msgs::MarkerArray> marker_sub(nh, "input_marker",1);
+
+  //boost::shared_ptr<message_filters::Synchronizer<MySyncPolicy> > sync_ = boost::make_shared<message_filters::Synchronizer<MySyncPolicy> >(100);
+  //message_filters::Synchronizer<MySyncPolicy> sync_(MySyncPolicy(10), box_sub, marker_sub);
+  sync_.connectInput(box_sub, marker_sub);
+  sync_.registerCallback(boost::bind(&calculate, _1, _2));
+
+  //message_filters::Synchronizer<MySyncPolicy> sync_(MySyncPolicy(10), box_sub, marker_sub);
+  //sync.registerCallback(boost::bind(&calculate, _1, _2));
+
+  //Create ROS publishers for output
+  dist_pub = nh.advertise<geometry_msgs::PointStamped> ("distance", 1);
+  nearest_marker_pub = nh.advertise<visualization_msgs::Marker> ("output_marker", 1);
+  
+  //Spin
+  // ros::Rate loop_rate(20);
+  // while(ros::ok()){
+  //   ros::spineOnce ();
+  //   loop_rate.sleep();
+  //ros::spine();
+
+  return 0;
+}
+ 
+
+
