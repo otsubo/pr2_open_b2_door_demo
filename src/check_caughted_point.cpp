e66d8cfa58201807b27168892708b094734c21de
Satoshi Otsubo <otsubo@jsk.imi.i.u-tokyo.ac.jp>
2017-03-29 23:53:57 +0900

    fix check node

--- src/check_caughted_point.cpp
+++ src/check_caughted_point.cpp
@@ -31,26 +31,18 @@ void calculate(
   //visualization_msgs::MarkerArray ma;
   jsk_recognition_msgs::EdgeArray new_edge_msg(*edge_array);
   jsk_recognition_msgs::Edge nearest_edge;
-  double dist;
-  std::vector<double> dist_vec;
-  double min_dist;
-  // ba.header = box->header;
-  // ba.pose = box->pose;
-  // ba.dimensions = box->dimensions;
-  //ma.markers.header = marker_array->header;
-  //ma.markers = marker_array->markers;
+  float dist;
+  std::vector<float> dist_vec;
+  float min_dist;
 
-  double b_x = new_bbox_msg.pose.position.x;
-  double b_y = new_bbox_msg.pose.position.y;
-  double b_z = new_bbox_msg.pose.position.z;
-  double e_x;
-  double e_y;
-  double e_z;
-  //ba.header = box->header;
-  //ba.box = box->box;
-  //ma.header = marker_array->header;
-  //ma.markers = marker_array->markers;
+  float b_x = new_bbox_msg.pose.position.x;
+  float b_y = new_bbox_msg.pose.position.y;
+  float b_z = new_bbox_msg.pose.position.z;
+  float e_x;
+  float e_y;
+  float e_z;
 
+  //calculate min distance and return nearest edge
   for(size_t i = 0; i < new_edge_msg.edges.size() ; i++) {
     min_dist = -100;
     e_x = new_edge_msg.edges[i].start_point.x;
@@ -61,8 +53,8 @@ void calculate(
       min_dist = dist;
     nearest_edge = new_edge_msg.edges[i];
   }
-  //Publish the data
-  dist_pub.publish (min_dist);
+  // //Publish the data
+  //dist_pub.publish (min_dist);
   nearest_edge_pub.publish (nearest_edge);
 }
 
@@ -71,36 +63,31 @@ int
 main(int argc, char** argv)
 {
 
-  //boost::mutex::scope_lock lock(mutex_);
+  boost::mutex::scoped_lock lock(mutex_);
   //Initialize ROS
   ros::init (argc, argv, "check_node");
   ros::NodeHandle nh;
   typedef message_filters::sync_policies::ApproximateTime<jsk_recognition_msgs::BoundingBox, jsk_recognition_msgs::EdgeArray> MySyncPolicy;
-  //boost::shared_ptr<message_filters::Synchronizer<MySyncPolicy> > sync_;
+
   //message filter
   message_filters::Subscriber<jsk_recognition_msgs::BoundingBox> box_sub(nh, "input_box",1);
   message_filters::Subscriber<jsk_recognition_msgs::EdgeArray> edge_sub(nh, "input_edge",1);
 
-  //boost::shared_ptr<message_filters::Synchronizer<MySyncPolicy> > sync_ = boost::make_shared<message_filters::Synchronizer<MySyncPolicy> >(100);
-  //message_filters::Synchronizer<MySyncPolicy> sync_(MySyncPolicy(10), box_sub, marker_sub);
-  //sync_.connectInput(box_sub, edge_sub);
-  //sync_.registerCallback(boost::bind(&calculate, _1, _2));
 
   message_filters::Synchronizer<MySyncPolicy> sync_(MySyncPolicy(10), box_sub, edge_sub);
   sync_.registerCallback(boost::bind(&calculate, _1, _2));
 
   //Create ROS publishers for output
-  dist_pub = nh.advertise<geometry_msgs::PointStamped> ("distance", 1);
+  //dist_pub = nh.advertise<geometry_msgs::PointStamped> ("distance", 1);
   nearest_edge_pub = nh.advertise<jsk_recognition_msgs::Edge> ("output_edge", 1);
   
   //Spin
-  // ros::Rate loop_rate(20);
-  // while(ros::ok()){
-  //   ros::spineOnce ();
-  //   loop_rate.sleep();
-  //ros::spine();
+  ros::Rate loop_rate(20);
+  while(ros::ok()){
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
-  return 0;
 }
  
 
