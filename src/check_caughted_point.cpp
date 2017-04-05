db4c3dd85314891231e31eaf4efd5cdfdedae6de
Satoshi Otsubo <otsubo@jsk.imi.i.u-tokyo.ac.jp>
2017-04-05 15:30:14 +0900

    update markerarray stamped

--- src/check_caughted_point.cpp
+++ src/check_caughted_point.cpp
@@ -3,8 +3,7 @@
 #include <visualization_msgs/MarkerArray.h>
 #include <jsk_recognition_msgs/BoundingBox.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
-#include <jsk_recognition_msgs/Edge.h>
-#include <jsk_recognition_msgs/EdgeArray.h>
+#include <jsk_recognition_msgs/MarkerArrayStamped.h>
 #include <geometry_msgs/PointStamped.h>
 #include <time.h>
 #include <vector>
@@ -15,22 +14,22 @@
 
 
 ros::Publisher dist_pub;
-ros::Publisher nearest_edge_pub;
+ros::Publisher nearest_marker_pub;
 boost::mutex mutex_;
 
 
 
 void calculate(
                const jsk_recognition_msgs::BoundingBox::ConstPtr& box,
-               const jsk_recognition_msgs::EdgeArray::ConstPtr& edge_array)
+               const jsk_recognition_msgs::MarkerArrayStamped::ConstPtr& marker_array)
 {
   //init input topic
 
   //std::count<<"calculate"<<std::endl;
   jsk_recognition_msgs::BoundingBox new_bbox_msg(*box);
   //visualization_msgs::MarkerArray ma;
-  jsk_recognition_msgs::EdgeArray new_edge_msg(*edge_array);
-  jsk_recognition_msgs::Edge nearest_edge;
+  jsk_recognition_msgs::MarkerArrayStamped new_marker_msg(*marker_array);
+  visualization_msgs::Marker nearest_marker;
   float dist;
   std::vector<float> dist_vec;
   float min_dist;
@@ -38,24 +37,24 @@ void calculate(
   float b_x = new_bbox_msg.pose.position.x;
   float b_y = new_bbox_msg.pose.position.y;
   float b_z = new_bbox_msg.pose.position.z;
-  float e_x;
-  float e_y;
-  float e_z;
+  float m_x;
+  float m_y;
+  float m_z;
 
   //calculate min distance and return nearest edge
-  for(size_t i = 0; i < new_edge_msg.edges.size() ; i++) {
+  for(size_t i = 0; i < new_marker_msg.markers.size() ; i++) {
     min_dist = -100;
-    e_x = new_edge_msg.edges[i].start_point.x;
-    e_y = new_edge_msg.edges[i].start_point.y;
-    e_z = new_edge_msg.edges[i].start_point.z;
-    dist = sqrt(pow(e_x - b_x, 2.0) + pow(e_y - b_y, 2.0) + pow(e_z - b_z, 2.0));
+    m_x = new_marker_msg.markers[i].pose.position.x;
+    m_y = new_marker_msg.markers[i].pose.position.y;
+    m_z = new_marker_msg.markers[i].pose.position.z;
+    dist = sqrt(pow(m_x - b_x, 2.0) + pow(m_y - b_y, 2.0) + pow(m_z - b_z, 2.0));
     if (dist < min_dist)
       min_dist = dist;
-    nearest_edge = new_edge_msg.edges[i];
+    nearest_marker = new_marker_msg.markers[i];
   }
   // //Publish the data
   //dist_pub.publish (min_dist);
-  nearest_edge_pub.publish (nearest_edge);
+  nearest_marker_pub.publish (nearest_marker);
 }
 
 
@@ -67,19 +66,19 @@ main(int argc, char** argv)
   //Initialize ROS
   ros::init (argc, argv, "check_node");
   ros::NodeHandle nh;
-  typedef message_filters::sync_policies::ApproximateTime<jsk_recognition_msgs::BoundingBox, jsk_recognition_msgs::EdgeArray> MySyncPolicy;
+  typedef message_filters::sync_policies::ApproximateTime<jsk_recognition_msgs::BoundingBox, jsk_recognition_msgs::MarkerArrayStamped> MySyncPolicy;
 
   //message filter
   message_filters::Subscriber<jsk_recognition_msgs::BoundingBox> box_sub(nh, "input_box",1);
-  message_filters::Subscriber<jsk_recognition_msgs::EdgeArray> edge_sub(nh, "input_edge",1);
+  message_filters::Subscriber<jsk_recognition_msgs::MarkerArrayStamped> marker_sub(nh, "input_marker",1);
 
 
-  message_filters::Synchronizer<MySyncPolicy> sync_(MySyncPolicy(10), box_sub, edge_sub);
+  message_filters::Synchronizer<MySyncPolicy> sync_(MySyncPolicy(10), box_sub, marker_sub);
   sync_.registerCallback(boost::bind(&calculate, _1, _2));
 
   //Create ROS publishers for output
   //dist_pub = nh.advertise<geometry_msgs::PointStamped> ("distance", 1);
-  nearest_edge_pub = nh.advertise<jsk_recognition_msgs::Edge> ("output_edge", 1);
+  nearest_marker_pub = nh.advertise<visualization_msgs::Marker> ("output_marker", 1);
   
   //Spin
   ros::Rate loop_rate(20);
